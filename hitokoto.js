d07eecb4bb00a8e16811e24f280ef6f78b2da683
jkfz187 <2403818500@qq.com>
2021-08-05 20:44:26 +0800

    git test

---
+++ hitokoto.js
@@ -0,0 +1,35 @@
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+sleep(500).then(() => $(function () {
+    var xhr = new XMLHttpRequest()
+    xhr.open('get', 'https://v1.hitokoto.cn')
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            var data = JSON.parse(xhr.responseText)
+            var hitokoto = document.getElementById('hitokoto')
+            hitokoto.innerText = data.hitokoto
+            var hitokoto_from = document.getElementById('hitokoto_from')
+            if (data.from == "internet" || data.from == "网络" || data.from == "原创" || data.from == "贴吧" || data.from == "网易云" || data.from == "网友" || data.from == "知乎") {
+                hitokoto_from.innerText = data.from
+            } else if (data.from == "来自网络") {
+                hitokoto_from.innerText = "网络"
+            } else if (data.from == "哲学") {
+                hitokoto_from.innerText = data.from_who
+                来自网络
+            } else {
+                if (data.from_who == null) {
+                    hitokoto_from.innerText = "《" + data.from + "》"
+                } else {
+                    hitokoto_from.innerText = "《" + data.from + "》" + data.from_who
+                }
+            }
+        } else {
+        document.getElementById('hitokoto').innerText = "Jiangxi University of Technology High School Grade 2018 Class 7"
+    }
+    document.getElementById('hitokoto').style.color = "var(--ft-light-or-dark)"
+    document.getElementById('hitokoto_from').style.color = "var(--ft-light-or-dark)"
+}
+    xhr.send()
+}))
---
+++ light_or_dark.js
@@ -0,0 +1,52 @@
+if (navigator.cookieEnabled) {
+    var cookie = document.cookie;
+    if (cookie == "mode=dark") {
+        dark()
+    } else if (cookie == "mode=light") {
+        light()
+    } else {
+        if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+            light()
+        } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            dark()
+        } else {
+            document.getElementById("no-light-or-dark").style.display = "block"
+        }
+    }
+} else {
+    document.getElementById("no-cookie").style.display = "inline"
+}
+
+
+
+function dark() {
+    document.documentElement.style.setProperty('--bg-light-or-dark', "#333333")
+    document.documentElement.style.setProperty('--bd-light-or-dark', "#252525")
+    document.documentElement.style.setProperty('--ft-light-or-dark', "#666666")
+    document.documentElement.style.setProperty('--im-light-or-dark', "opacity(50%)")
+    //dqh
+    document.documentElement.style.setProperty('--dqh', "invert(100%)")
+    //cookie
+    document.cookie = "mode=dark;path=/";
+    //czp
+    document.documentElement.style.setProperty('--bg-cl', '#333333')
+    document.documentElement.style.setProperty('--ft-cl', '#666666')
+    document.getElementById("czp_dark").style.display = "none"
+    document.getElementById("czp_light").style.display = "inline"
+}
+
+function light() {
+    document.documentElement.style.setProperty('--bg-light-or-dark', "#f8f9fb")
+    document.documentElement.style.setProperty('--bd-light-or-dark', "white")
+    document.documentElement.style.setProperty('--ft-light-or-dark', "black")
+    document.documentElement.style.setProperty('--im-light-or-dark', "opacity(100%)")
+    //dqh
+    document.documentElement.style.setProperty('--dqh', "invert(0%)")
+    //cookie
+    document.cookie = "mode=light;path=/";
+    //czp
+    document.documentElement.style.setProperty('--bg-cl', '#FAF9DE')
+    document.documentElement.style.setProperty('--ft-cl', 'black')
+    document.getElementById("czp_light").style.display = "none"
+    document.getElementById("czp_dark").style.display = "inline"
+}
